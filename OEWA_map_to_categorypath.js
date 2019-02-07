935d6561231af9e88a7ae1aa34e571f58784ad80
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2019-02-07 08:11:39 +0100

    added BBX OEWA extension for mapping page identifiers to oewa categorypaths

---
+++ OEWA_map_to_categorypath.js
@@ -0,0 +1,51 @@
+if (a === "view") {
+    if (b.event_name === "contact_contact") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else if (b.event_name === "terms_conditions_general") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else if (b.event_name === "ad_rules") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else if (b.event_name === "privacy_policy") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else if (b.event_name === "imprint") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else if (b.event_name === "terms_conditions") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else if (b.event_name === "press_about_willhaben") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else if (b.event_name === "press_presstext") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else if (b.event_name === "press_download") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else if (b.event_name === "security_hints_overview") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else if (b.event_name === "security_hints_buy") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else if (b.event_name === "security_hints_account") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else if (b.event_name === "security_hints_sell") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else if (b.event_name === "security_hints_puppy") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else if (b.event_name === "mywillhaben_myprofile") {
+        b.oewa_categorypath_sktg = "Service/Unternehmenskommunikation/Unternehmenskommunikation";
+        b.oewa_categorypath_pageid = "";
+    } else {
+        // ignore unknown pages
+        return false;
+    }
+}
