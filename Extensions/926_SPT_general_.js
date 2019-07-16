5fff41b60dacf65c36a924af0aecb67aafc08af4
daniel.zimmermann@willhaben.at <daniel.zimmermann@willhaben.at>
2019-07-16 14:59:47 +0200

    WHIAD-24313

--- Extensions/926_SPT_general_.js
+++ Extensions/926_SPT_general_.js
@@ -1,18 +1,138 @@
 var willhabenSPT = {} | willhabenSPT;
 
 willhabenSPT = {
+
+    EVENTS: {
+        AD_VIEW: 'adview',
+        CONTACT_SELLER_CHAT_CONFIRMATION: 'contact_seller_chat_confirmation',
+        CALL_BUTTON: 'call_button',
+        K_G_CHAT: 'K_G_chat'
+    },
+
+    B_PROPS: {
+        AD_TYPE_ID: 'ad_type_id',
+        EVENT_NAME: 'event_name',
+        SPT_CUSTOM: 'spt_custom'
+    },
+
+    utilities: {
+        appendSPTCustom: function (name, value) {
+            b[willhabenSPT.B_PROPS.SPT_CUSTOM] = b[willhabenSPT.B_PROPS.SPT_CUSTOM] || {};
+            var sptCustom = JSON.parse(b[willhabenSPT.B_PROPS.SPT_CUSTOM]);
+            sptCustom['object'] = sptCustom['object'] || {};
+            sptCustom['object'][name] = value;
+            b[willhabenSPT.B_PROPS.SPT_CUSTOM] = JSON.stringify(sptCustom);
+        }
+    },
+
     classifiedAd: {
-        includePublisher: function() {
-            if (b && b['event_name'] === 'adview') {
+
+        includeCategories: function () {
+
+            function make_marketplace_categories() {
+
+                var rootCategoryName;
+                var rootCategoryId;
+
+                switch (b[willhabenSPT.B_PROPS.AD_TYPE_ID]) {
+                    case "20":
+                    case "21":
+                    case "25":
+                    case "26":
+                        rootCategoryName = 'Motor';
+                        break;
+                    case "66":
+                    case "67":
+                    case "68":
+                    case "69":
+                        rootCategoryName = 'Generalist';
+                        rootCategoryId = 2;
+                        break;
+                    case "40":
+                    case "49":
+                        rootCategoryName = 'Jobs';
+                        break;
+                    default:
+                        rootCategoryName = 'Realestate';
+                        rootCategoryId = 7274;
+                }
+
+                var categories = [];
+
+                if (b['category_level_5']) {
+                    categories.push(map_marketplace_category(b['category_level_id_5'], b['category_level_5'], 5, rootCategoryName));
+                }
+
+                if (b['category_level_4']) {
+                    categories.push(map_marketplace_category(b['category_level_id_4'], b['category_level_4'], 4, rootCategoryName), b[willhabenSPT.B_PROPS.AD_TYPE_ID]);
+                }
+
+                if (b['category_level_3']) {
+                    categories.push(map_marketplace_category(b['category_level_id_3'], b['category_level_3'], 3, rootCategoryName), b[willhabenSPT.B_PROPS.AD_TYPE_ID]);
+                }
+
+                if (b['category_level_2']) {
+                    categories.push(map_marketplace_category(b['category_level_id_2'], b['category_level_2'], 2, rootCategoryName), b[willhabenSPT.B_PROPS.AD_TYPE_ID]);
+                }
+
+                if (b['category_level_1']) {
+                    categories.push(map_marketplace_category(b['category_level_id_1'], b['category_level_1'], 1, rootCategoryName), b[willhabenSPT.B_PROPS.AD_TYPE_ID]);
+                }
+
+                categories.push(map_marketplace_category(rootCategoryId, rootCategoryName, 0, rootCategoryName));
+
+                return categories;
+            }
+
+            function map_marketplace_category(id, name, level, rootCategoryName, adTypeId) {
+
+                var category = {};
+                category["@type"] = "MarketplaceCategory";
+                category["localName"] = name;
+                category["level"] = level;
+                category["name"] = name;
+
+                var localId =  "sdrn:willhabenat:category:" + rootCategoryName;
+                if (rootCategoryName == 'Motor') {
+                    if (level !== 0) {
+                        localId = localId + ":" + adTypeId;
+                    }
+                } else {
+                    localId = localId + ":" + id;
+                }
+                category["localId"] = localId;
+
+                return category;
+            }
+
+
+            switch (b[willhabenSPT.B_PROPS.EVENT_NAME].toString().toLowerCase()) {
+
+                case willhabenSPT.EVENTS.AD_VIEW:
+                    willhabenSPT.utilities.appendSPTCustom('categories', make_marketplace_categories());
+                    break;
+
+                case willhabenSPT.EVENTS.CONTACT_SELLER_CHAT_CONFIRMATION:
+                case willhabenSPT.EVENTS.CALL_BUTTON:
+                case willhabenSPT.EVENTS.K_G_CHAT:
+                    b.spt_in_reply_to = {};
+                    b.spt_in_reply_to.categories = make_marketplace_categories();
+                    break;
+            }
+        },
+
+        includePublisher: function () {
+            if (b && b[willhabenSPT.B_PROPS.EVENT_NAME] === 'adview') {
                 var publisher = {
-                   "@id": "sdrn:willhaben:user:",
-                   "@type": "Account",
+                    "@id": "sdrn:willhaben:user:" + b.seller_uuid,
+                    "@type": "Account",
                 };
-                b.append_spt_custom_object('publisher', publisher);
+                willhabenSPT.utilities.appendSPTCustom('publisher', publisher);
             }
         }
     },
-    build: function() {
+    build: function () {
+        willhabenSPT.classifiedAd.includeCategories();
         willhabenSPT.classifiedAd.includePublisher();
     }
 };
