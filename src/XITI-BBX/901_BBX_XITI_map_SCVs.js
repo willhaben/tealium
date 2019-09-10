3693865934ba919f648999cbdd9a2020b30df615
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2019-09-10 14:36:07 +0200

    BBX-899 set xiti_x1 parameter when is_private is given

--- src/XITI-BBX/901_BBX_XITI_map_SCVs.js
+++ src/XITI-BBX/901_BBX_XITI_map_SCVs.js
@@ -9,6 +9,10 @@ if (!b.client || b.client.toLowerCase() !== "bbx") {
 // do not set xiti SCVs for clicks, otherwise additional page views will be tagged
 if (a == "view") {
     // INFO xiti_x4 is set in extension 788
+    if (typeof b.is_private !== "undefined") {
+        // 1=C2C, 2=B2C
+        b.xiti_x1 = b.is_private === "true" ? "1" : "2";
+    }
     b.xiti_x5 = b.region_level_2;
     b.xiti_x6 = b.ad_id;
     b.xiti_x7 = "willhaben";
