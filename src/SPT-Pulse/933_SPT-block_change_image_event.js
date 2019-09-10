0d4f46e6d1a76d1cc8b85472d028dbf863b871b6
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2019-09-10 15:32:52 +0200

    BBX-1095 MWEB-3021 WHIAD-24606 also ignore view_image event in mweb gallery

--- src/SPT-Pulse/933_SPT-block_change_image_event.js
+++ src/SPT-Pulse/933_SPT-block_change_image_event.js
@@ -1,7 +1,10 @@
 var client = (b.client || "").toLowerCase();
 if (
     a === "view" &&
-    ((client === "bbx" && b.event_name === "adimage_view") || (client === "desktop" && b.event_name === "ad_img") || (client === "mweb" && b.event_name === "adimg"))
+    ((client === "bbx" && b.event_name === "adimage_view") ||
+        (client === "desktop" && b.event_name === "ad_img") ||
+        (client === "mweb" && b.event_name === "adimg") ||
+        (client === "mweb" && b.event_name === "view_image"))
 ) {
     // do not track change image events in Pulse
     return false;
