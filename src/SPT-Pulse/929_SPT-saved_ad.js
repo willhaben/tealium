4e2cc7d96256544af01451bbf1b8b6cdfc0b5139
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2019-09-10 11:29:20 +0200

    BBX-899 added saved ad SPT mapping for addetail_favorite_ad_saved and addetail_favorite_ad_unsaved events

--- src/SPT-Pulse/929_SPT-saved_ad.js
+++ src/SPT-Pulse/929_SPT-saved_ad.js
@@ -1,23 +1,32 @@
-if (typeof b.event_name !== "undefined" && (b.event_name === "favorite_ad" || b.event_name === "favorite_ad_search_result" || b.event_name === "delete_favorite_ad") && a !== "view") {
-    pulse(function (tracker){
-        tracker.evaluateEventInputs().then(function (eventDefaults) {
+if (
+    typeof b.event_name !== "undefined" &&
+    (b.event_name === "favorite_ad" ||
+        b.event_name === "favorite_ad_search_result" ||
+        b.event_name === "delete_favorite_ad" ||
+        b.event_name === "addetail_favorite_ad_saved" ||
+        b.event_name === "addetail_favorite_ad_unsaved") &&
+    a !== "view"
+) {
+    pulse(function(tracker) {
+        tracker.evaluateEventInputs().then(function(eventDefaults) {
+            var isSaved = b.favorite_ad_saved || b.event_name === "addetail_favorite_ad_saved";
             var clickEvent = {
-                name: b.favorite_ad_saved ? "Ad saved": "Ad unsaved",
-                type: b.favorite_ad_saved ? "Save": "Unsave",
-                object: eventDefaults.object
+                name: isSaved ? "Ad saved" : "Ad unsaved",
+                type: isSaved ? "Save" : "Unsave",
+                object: eventDefaults.object,
             };
             //if this was a search page find the ad in the listing items
             var items;
 
             if (eventDefaults.object.items) {
                 items = eventDefaults.object.items;
-            } else if (b['spt_items']) {
-                items = b['spt_items']
+            } else if (b["spt_items"]) {
+                items = b["spt_items"];
             }
 
             if (items) {
                 var ads = items;
-                for (var i= 0; i < ads.length; i++) {
+                for (var i = 0; i < ads.length; i++) {
                     if (ads[i].adId == b.favorite_ad) {
                         clickEvent.object = ads[i];
                         if (clickEvent.object.price === 0 && clickEvent.object.adType === "sell") {
