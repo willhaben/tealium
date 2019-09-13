6ac319f1ea3b04bb01be977ef9366f6c46df0369
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2019-09-13 09:29:37 +0200

    BBX-1182 use is_private for calculating publisherType for phone number clicked events, instead of the ad_publishertype variable which is just hardcoded provided by mweb

--- src/SPT-Pulse/840_SPT_phone_number_called.js
+++ src/SPT-Pulse/840_SPT_phone_number_called.js
@@ -2,7 +2,8 @@ var client = (b.client || "").toLowerCase();
 if (client === "desktop" || client === "mweb" || client === "bbx") {
     if (a === "link" && (b.event_name == "call_number" || b.event_name == "call_button" || b.event_name == "addetail_call_phone_click")) {
         var classifiedId = b.ad_id + "";
-        //var scroll = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+        var isPrivate = b.is_private === "true";
+
         pulse(function(tracker) {
             tracker.evaluateEventInputs().then(function(eventDefaults) {
                 var clickEvent = {
@@ -19,7 +20,7 @@ if (client === "desktop" || client === "mweb" || client === "bbx") {
                             category: b.category_level_2 ? b.category_level_1 + " > " + b.category_level_2 : b.category_level_1,
                             categories: eventDefaults && eventDefaults.object ? eventDefaults.object.categories : undefined,
                             adType: b.ad_type_schibsted,
-                            publisherType: b.ad_publishertype,
+                            publisherType: isPrivate ? "private" : "pro",
                             contentId: b.ad_id,
                             name: b.ad_title,
                             publisher: eventDefaults && eventDefaults.object ? eventDefaults.object.publisher : undefined,
