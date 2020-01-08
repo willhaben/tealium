1f47ed60803e616319a580a0490c32b4810e1ac4
Florian Rauch <florian.rauch@willhaben.at>
2020-01-08 14:40:32 +0100

    WHIAD-25119 MWEB-3089: combined the 2 if clauses

--- src/SPT-Pulse/786_SPT-ad_type_mapping_extension_for_give.js
+++ src/SPT-Pulse/786_SPT-ad_type_mapping_extension_for_give.js
@@ -3,10 +3,6 @@ if (b["ad_type_id"] === "67" && b["price"] && parseFloat(b["price"]) === 0) {
     b["ad_type_schibsted"] = "give";
 }
 
-if (b["price"] === undefined) {
-    b["spt_price"] = 0;
-}
-
-if (b["price"] && isNaN(parseFloat(b["price"]))) {
+if (!b["price"] || isNaN(parseFloat(b["price"]))) {
     b["spt_price"] = 0;
 }
