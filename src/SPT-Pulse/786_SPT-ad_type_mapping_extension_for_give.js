8374641b23b9d0c74af0283298dc84c899855b74
Florian Rauch <florian.rauch@willhaben.at>
2020-01-08 14:32:33 +0100

    WHIAD-25119 MWEB-3089: if price is undefined then set price to 0 to avoid breaking the schema

--- src/SPT-Pulse/786_SPT-ad_type_mapping_extension_for_give.js
+++ src/SPT-Pulse/786_SPT-ad_type_mapping_extension_for_give.js
@@ -3,6 +3,10 @@ if (b["ad_type_id"] === "67" && b["price"] && parseFloat(b["price"]) === 0) {
     b["ad_type_schibsted"] = "give";
 }
 
+if (b["price"] === undefined) {
+    b["spt_price"] = 0;
+}
+
 if (b["price"] && isNaN(parseFloat(b["price"]))) {
     b["spt_price"] = 0;
 }
