8c08a4820d5bf5c9216777d788bc9f4028afa2f1
redlmade <madeleine.redl@willhaben.at>
2020-03-03 11:51:04 +0100

    JOBS-5038 removed old click mapping, only map payment success page

--- src/Facebook-Jobs-Employer/937_Facebook_click-mapping-for-private-employer-pages.js
+++ src/Facebook-Jobs-Employer/937_Facebook_click-mapping-for-private-employer-pages.js
@@ -4,26 +4,8 @@ if (!b.client || b.client.toLowerCase() !== "jobs") {
 
 if (a === "link" && b.application === "employer") {
 
-    if (b.application_view === "purchase") {
-        if (b.event_name === "purchaseBuy") {
-            b.fb_event = "Purchase";
-            b.currency = "EUR";
-            b.price = b.product_price;
-            b.product_quantity = b.product_quantity;
-        } else {
-            // unused - do not send facebook events
-            return false;
-        }
-    } else if (b.application_view === "checkout") {
-        if (b.event_name === "checkoutPay") {
-            b.fb_event = "Purchase";
-            b.currency = "EUR";
-            b.price = b.product_price;
-            b.product_quantity = b.product_quantity;
-        } else {
-            // unused - do not send facebook events
-            return false;
-        }
+    if (b.application_view === "purchasePaymentSuccess") {
+        b.fb_event = "Purchase";
     } else {
         // unused - do not send facebook events
         return false;
