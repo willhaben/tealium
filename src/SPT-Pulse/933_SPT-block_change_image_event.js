486108b81f8b65ae27028e6cbc0168c5bf00dd7f
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2020-06-16 15:32:25 +0200

    BBX-2047 the events adimage_view and adimage_view_fullscreen have been removed from bbx, therefore they can be removed from the pulse event filter

--- src/SPT-Pulse/933_SPT-block_change_image_event.js
+++ src/SPT-Pulse/933_SPT-block_change_image_event.js
@@ -1,11 +1,7 @@
 var client = (b.client || "").toLowerCase();
 if (
     a === "view" &&
-    ((client === "bbx" && b.event_name === "adimage_view") ||
-        (client === "bbx" && b.event_name === "adimage_view_fullscreen") ||
-        (client === "desktop" && b.event_name === "ad_img") ||
-        (client === "mweb" && b.event_name === "adimg") ||
-        (client === "mweb" && b.event_name === "view_image"))
+    ((client === "desktop" && b.event_name === "ad_img") || (client === "mweb" && b.event_name === "adimg") || (client === "mweb" && b.event_name === "view_image"))
 ) {
     // do not track change image events in Pulse
     return false;
