fdce6178312c5c2ec8a29e74f2c9786536675543
Lukas Schneider <lukas.schneider@willhaben.at>
2020-07-14 13:02:38 +0200

    JOBS-5435 * fixed google conversion tracking

--- src/Google-Jobs-Employer/943_Google_view-mapping-for-private-employer-pages.js
+++ src/Google-Jobs-Employer/943_Google_view-mapping-for-private-employer-pages.js
@@ -3,15 +3,13 @@ if (!b.client || b.client.toLowerCase() !== "jobs") {
 }
 
 if (a === "view" && b.application === "employer") {
-    if (b.application_view === "purchasePaymentInitiated") {
-        b.order_id = "";
+    if (b.application_view === "purchasePaymentSuccess") {
+         b.order_id = "";
         b.currency = "EUR";
         b.price = b.product_price;
+        b.conversion_id = "806155102";
+        b.conversion_label = "ZaBECPyS7aoBEN7ms4AD";
     } else {
-        // unused - do not send events to Google
         return false;
     }
-} else {
-    // unused - do not send events to Google
-    return false;
 }
